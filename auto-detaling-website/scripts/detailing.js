e6824f0b8dc346267d8331ca9a8d98082a195871
Ayydevon <elicyrus.00@gmail.com>
2026-06-29 20:12:42 -0400

    created footnote for website, created contact section for website

--- auto-detaling-website/scripts/detailing.js
+++ auto-detaling-website/scripts/detailing.js
@@ -1,2 +1,10 @@
 import { scrollAnimation } from "../../Main website/scripts/portfolio.js";
 scrollAnimation()
+function themeChanger(){
+    let html;
+    let darkMode = false;
+    const head = document.querySelector('head');
+    if (darkMode == false){
+        
+    }
+}
