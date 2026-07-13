3e192df1fa9b5c276a3077041763046c4841f429
Ayydevon <elicyrus.00@gmail.com>
2026-07-13 19:13:11 -0400

    created css for contact section

--- wrap-shop-website/scripts/wrap.js
+++ wrap-shop-website/scripts/wrap.js
@@ -17,7 +17,62 @@ showMore.addEventListener('click', ()=>{
          })
         }
     })
-    
+   const submitButton = document.querySelector('.contact-form')
+   submitButton.addEventListener('submit', (e)=> {
+    e.preventDefault()
+   })
    
 
-   
+   let html = 
+function themeChanger(){
+
+const head = document.querySelector('head')
+let darkMode = false;
+let position = 0
+const speed = 2
+const displayDivWidth = document.querySelector('.display-div').offsetWidth
+const themeButton = document.querySelector('.theme-button')
+function themeChanger(){
+function themeChangerInner (html,head){
+head.innerHTML = html
+}
+themeButton.addEventListener('click', ()=> {
+    
+    if (darkMode){
+        themeChangerInner(lightHTML,head)
+       animateBack()
+        darkMode = false
+    }
+    else{
+        themeChangerInner(darkHTML,head)
+        document.querySelector('.back-arrow').src = 'images/arrow-left-2.png'
+       animate()
+        darkMode = true
+        
+
+    }
+})
+
+}
+
+function animate(){
+    
+     position += speed
+     if (position <= displayDivWidth ){
+        document.querySelector('.display-div').style.transform = `translateX(${position}px)`
+            requestAnimationFrame(animate)
+        }
+        document.querySelector('.header-button-image').src = 'images/moon.png'
+}
+function animateBack (){
+    position -= speed
+    if (position > 0 ){
+        document.querySelector('.display-div').style.transform = `translateX(${position}px)`
+            requestAnimationFrame(animateBack)
+        }
+    document.querySelector('.header-button-image').src = 'images/sun.png'
+}
+
+themeChanger()
+
+}
